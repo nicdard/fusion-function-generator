35b21ae77a8b00f9ddc8b381435454751abd6a79
Nicola Dardanis <nicdard@gmail.com>
2022-03-30 15:00:35 +0200

    feat: Add reverse theory map construction

--- operators/gen_configuration.py
+++ operators/gen_configuration.py
@@ -1,4 +1,7 @@
-from typing import Dict, Union
+from cgi import parse_multipart
+from copy import deepcopy
+from re import A
+from typing import Dict, List, Union
 
 # A map from theories name to theory operation.
 # Each operation specifies either the number of input parameters (having the same theory operator type).
@@ -31,3 +34,28 @@ theories: Dict[str, Dict[str, Union[str, int]]] = {
         "Equality": 2
     }
 }
+
+
+def reverse_theory_dict() -> Dict[int, Dict[str, List[str]]]:
+    """
+    Returns a reverse dictionary from the (arity, theory) to operators.
+    """
+    parse_arity = lambda params : len(params) != 0 if len(params.split(",")) else 0
+    arity_map = {}
+    for theory in theories.keys():
+        theory = theories[theory]
+        # We assume to have at least one bnary operator in a theory, 
+        # since we want to be able to fuse functions. Including theories
+        # without such an operation would be useless.
+        for op in theory:
+            args = theory[op]
+            arity = parse_arity(args)
+            if arity in arity_map.keys():
+                if theory in arity_map[arity]:
+                    arity_map[arity][theory].append(op)
+                else:
+                    arity_map[arity][theory] = [op]
+            else:
+                arity_map[arity] = {}
+                arity_map[arity][theory] = []
+    return arity_map
