d0493ad0bab259cc088a7c766ae422421e717279
Nicola Dardanis <nicdard@gmail.com>
2022-04-05 02:13:31 +0200

    feat: Call emitter in main

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-import random
-
 from operators import gen_configuration
-from operators.dot_visitor import DotVisitor
-from operators.printer_visitor import PrinterVisitor
-from operators.rewrite_visitor import RewriteVisitor
+from operators.emitter.yinyang_emitter import emit
 from operators.tree_generation import generate_tree
 
 
 def main():
-    rewriter = RewriteVisitor()
-    printer = PrinterVisitor()
-    dot_exporter = DotVisitor()
-
     operator_types = gen_configuration.get_theories()
     print(operator_types)
 
     root_type = operator_types[2]
-    # root_type = random.choice(operator_types)
-    # print(root_type)
+    t = generate_tree(root_type, 25, 2)
+    emit(t)
 
-    t = generate_tree(root_type, 100, 3)
-    print(t.accept(printer))
-
-    with open('tree.dot', 'w') as file:
-        print(t.accept(dot_exporter), file=file)
-
-    for i, inverse in enumerate(t.accept(rewriter)):
-        print(inverse.accept(printer))
-
-        with open(f'inv_tree_{inverse.operator_1.name}.dot', 'w') as file:
-            print(inverse.accept(dot_exporter), file=file)
+    # with open('tree.dot', 'w') as file:
+    #     print(t.accept(dot_exporter), file=file)
+    # 
+    # for i, inverse in enumerate(t.accept(rewriter)):
+    #     with open(f'inv_tree_{i+1}.dot', 'w') as file:
+    #         print(inverse.accept(dot_exporter), file=file)
 
 
 if __name__ == '__main__':
