5df3502ac230ea88f6cf26719bc6dc062264e8c7
Nicola Dardanis <nicdard@gmail.com>
2022-04-13 20:05:06 +0200

    feat: add api option to emit either constant names or values

--- src/emitter/yinyang_emitter.py
+++ src/emitter/yinyang_emitter.py
@@ -1,10 +1,12 @@
 from src.operators.generic import Operator
+from src.visitors.constant_visitor import ConstantVisitor
 from src.visitors.printer_visitor import PrinterVisitor
 from src.visitors.rewrite_visitor import RewriteVisitor
+from src.visitors.symbolic_printer_visitor import SymbolicPrinterVisitor
 from src.visitors.variable_visitor import VariableVisitor
 
 
-def emit(tree: Operator, file):
+def emit(tree: Operator, file, is_symbolic: bool = True):
     """
     Emits the fusion functions and its inverses to yinyang's configuration file:
         #begin
@@ -18,18 +20,25 @@ def emit(tree: Operator, file):
         #end
     For more information on the format please visit:
     https://yinyang.readthedocs.io/en/latest/fusion.html#fusion-functions
+
+    is_symbolic: when traversing constants, emits either the names (true) or the values (false).
     """
 
     rewriter = RewriteVisitor()
-    printer = PrinterVisitor()
+    printer = SymbolicPrinterVisitor() if is_symbolic else PrinterVisitor()
     variables = tree.accept(VariableVisitor())
+    constants = tree.accept(ConstantVisitor()) if is_symbolic else {}
 
     # Block begin
     print("#begin", file=file)
 
     # Variable declarations
-    for variable, definition in variables.items():
-        print(f"(declare-const {variable} {definition})", file=file)
+    for variable, type in variables.items():
+        print(f"(declare-const {variable} {type})", file=file)
+
+    # Constant declarations
+    for constant, type in constants.items():
+        print(f"(declare-const {constant} {type})", file=file)
 
     # Fusion function
     print(f"(assert {tree.accept(printer)})", file=file)
