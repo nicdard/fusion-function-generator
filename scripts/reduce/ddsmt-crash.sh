6ea154b574fffd0679127a30e29abceab86372df
Nicola Dardanis <nicdard@gmail.com>
2022-05-14 11:39:40 +0200

    refactor: directly call solver to check crash and execute in foreground

--- scripts/reduce/ddsmt-crash.sh
+++ scripts/reduce/ddsmt-crash.sh
@@ -5,7 +5,7 @@ FILE=$1
 
 echo
 echo ======================
-echo "Download z3 latest"
+echo "Download z3-4.8.17-x64-glibc-2.31"
 if [ -d "./z3-4.8.17-x64-glibc-2.31" ]
 then
 	echo "...z3-4.8.17-x64-glibc-2.31 folder exists, skip."
@@ -15,30 +15,8 @@ else
 	rm z3-4.8.17-x64-glibc-2.31.zip
 fi
 Z3=$(readlink -f z3-4.8.17-x64-glibc-2.31/bin/z3)
-# Verify z3 works
-$Z3 -h
-
-echo
-echo ======================
-echo "Download CVC5 latest"
-if [ -f "./cvc5-Linux" ]
-then
-	echo "...cvc5-Linux exists, skip."
-else
-	wget https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.0/cvc5-Linux
-	chmod +x cvc5-Linux
-	# Verify cvc5 works
-	./cvc5-Linux -h
-fi
-
-if [ -f "check-crash.sh" ]
-then
-  chmod +x check-crash.sh
-else
-  exit 1
-fi
 
 # install ddsmt.
 pip3 install ddsmt
 python3 -m ddsmt --version
-python3 -m ddsmt -v $FILE $FILE.reduced check-crash.sh &
+python3 -m ddsmt -v --ignore-output $FILE $FILE.reduced $Z3
--- scripts/reduce/ddsmt.sh
+++ scripts/reduce/ddsmt.sh
@@ -5,7 +5,7 @@ FILE=$1
 
 echo
 echo ======================
-echo "Download z3 latest"
+echo "Download z3-4.8.17-x64-glibc-2.31"
 if [ -d "./z3-4.8.17-x64-glibc-2.31" ]
 then
 	echo "...z3-4.8.17-x64-glibc-2.31 folder exists, skip."
@@ -18,22 +18,9 @@ Z3=$(readlink -f z3-4.8.17-x64-glibc-2.31/bin/z3)
 # Verify z3 works
 $Z3 -h
 
-echo
-echo ======================
-echo "Download CVC5 latest"
-if [ -f "./cvc5-Linux" ]
-then
-	echo "...cvc5-Linux exists, skip."
-else
-	wget https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.0/cvc5-Linux
-	chmod +x cvc5-Linux
-	# Verify cvc5 works
-	./cvc5-Linux -h
-fi
-
 # install ddsmt.
 pip3 install ddsmt
 python3 -m ddsmt --version
 # make sure resut_differ.py is executable.
 chmod +x result_differs.py
-python3 -m ddsmt -v $FILE $FILE.reduced result_differs.py &
+python3 -m ddsmt -v $FILE $FILE.reduced result_differs.py
