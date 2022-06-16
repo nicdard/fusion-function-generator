e6e44c47052018bed09e8269d7f9c6e45c8865ea
Lucas Weitzendorf <lweitzendorf@student.ethz.ch>
2022-06-17 00:45:44 +0200

    refactor: changed generation to be more readable and reflect the pseudocode of the report

--- src/gen/tree_generation.py
+++ src/gen/tree_generation.py
@@ -56,32 +56,25 @@ def _generate_arity_tree(size: int, arities: List[int], min_leaves: int):
     tree = []
     rem_leaves = min_leaves
     min_op_arity = min(arities)
-    max_op_arity = max(arities)
-
-    def expected_nodes():
-        return sum(tree) + 1
-
-    def missing_nodes():
-        return expected_nodes() - len(tree)
 
     while len(tree) < size:
-        remaining = size - expected_nodes()
+        max_arity = size - sum(tree) - 1
 
-        if remaining in range(1, min_op_arity):
-            discrepancy = min_op_arity - remaining
+        if 0 < max_arity < min_op_arity:
+            discrepancy = min_op_arity - max_arity
             print(
                 f"Cannot match tree size {size} exactly, increasing to {size + discrepancy}...")
             size += discrepancy
-            remaining += discrepancy
+            max_arity += discrepancy
 
         rem_operators = size - len(tree) - rem_leaves
-        max_sub_leaves = (rem_operators - 1) * (max_op_arity - 1) + 1
-        min_arity = max(1, rem_leaves - missing_nodes() - max_sub_leaves + 2)
+        max_sub_leaves = (rem_operators - 1) * (max(arities) - 1) + 1
+        min_arity = rem_leaves - (sum(tree) - len(tree)) - max_sub_leaves + 1
 
         branching_choices = [
-            n for n in arities if n in range(min_arity, remaining+1)]
+            n for n in arities if (min_arity <= n <= max_arity)]
 
-        if not (missing_nodes() == 1 and remaining > 0):
+        if not (sum(tree) == len(tree) and max_arity > 0):
             branching_choices.extend([0, 0])  # two different leaf operators
 
         arity = random.choice(branching_choices)
